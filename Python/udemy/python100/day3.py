c4c40f1e8c70367a41d1774e527e076468f14751
Shindig8888 <164306017+Shindig8888@users.noreply.github.com>
2024-03-22 12:32:38 +0000

    3일차 다안끝냄

---
+++ Python/udemy/python100/day3.py
@@ -0,0 +1,54 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? : "))
+
+# if height >= 120:
+#   print("You can ride the rollercoaster!")
+# else:
+#   print("Sorry, you have to grow taller before you can ride.")
+
+###
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? : "))
+
+# if height >= 120:
+#   age = int(input("What is your age? : "))
+#   if age <=12:
+#     print("please pay $5")
+#   elif age<=18:
+#     print("please pay $7")
+#   else:
+#     print("please pay $12")
+# else:
+#   print("Sorry, you have to grow taller before you can ride.")
+
+###
+
+# bmi = weight / height**2
+# if True:
+#   if bmi <18.5:
+#     print (f"Your BMI is {bmi}, you are underweight."
+#   elif bmi<25:
+#     print (f"Your BMI is {bmi}, you are normal weight."
+#   elif bmi<30:
+#     print (f"Your BMI is {bmi}, you are slightly overweight."
+#   elif bmi<35:
+#     print (f"Your BMI is {bmi}, you are obese."
+#   else:
+#     print (f"Your BMI is {bmi}, you are clinically obese."
+# else: break
+
+# # Which year do you want to check?
+# year = int(input())
+# # 🚨 Don't change the code above 👆
+
+# # Write your code below this line 👇
+
+# if year%100 ==0 and year%400 ==0:
+#   print("Leap year")
+# elif year%100 ==0 and year%400 !=0:
+#   print("Not leap year")
+# elif year%4==0:
+#   print("Leap year")
+# else:
+#   print("Not leap year")
