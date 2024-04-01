3018bb554c48e515ea3e049d6435f3ddb93a2c25
Shindig <hindig8888@proton.me>
2024-04-01 19:52:15 +0900

    vscode에서도 어떻게 된다!

--- rust/ch1_6.rs
+++ rust/ch1_6.rs
@@ -75,7 +75,7 @@
 fn main(){
     let my_number = 9.6; //f64
     let other_number = 9; //i32
-    println!("{}", my_number + other_number as f64)
+    println!("my number sum is : {}", my_number + other_number as f64)
 
     /*float는 보통 f64를 사용*/
 }
