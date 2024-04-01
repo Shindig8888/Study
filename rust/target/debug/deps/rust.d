4425391bce1c5b39c205f212419a178336c65a6c
Shindig <hindig8888@proton.me>
2024-04-01 18:17:54 +0900

    rust

---
+++ rust/target/debug/deps/rust.d
@@ -0,0 +1,5 @@
+C:\Users\claix\OneDrive\Desktop\codes\Study\rust\target\debug\deps\rust.exe: src\main.rs
+
+C:\Users\claix\OneDrive\Desktop\codes\Study\rust\target\debug\deps\rust.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+C:\Users\claix\OneDrive\Desktop\codes\Study\rust\target\debug\rust.exe: C:\Users\claix\OneDrive\Desktop\codes\Study\rust\src\main.rs
