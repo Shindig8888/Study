2e647233870a5df337eca570114b0b74a4a3be40
Shindig8888 <164306017+Shindig8888@users.noreply.github.com>
2024-04-01 05:21:50 +0000

    day14

--- Python/udemy/python100/day14.py
+++ Python/udemy/python100/day14.py
@@ -1,16 +1,11 @@
-import data
+import data #리스트들
+from replit import clear #리플렛_클리어
 
 def gen_random_person(dic_list, end):
-    try:
-        import random
-        rand_index = random.randint(0, len(dic_list) - 1)
-        poplist = dic_list.pop(rand_index)
-        return poplist
-    except ValueError:
-        print("\nYou got it all right!")
-        poplist = {}
-        return poplist
-
+    import random
+    rand_index = random.randint(0, len(dic_list) - 1)
+    poplist = dic_list.pop(rand_index)
+    return poplist#랜덤뽑기
 
 def q_more_followers():
     while True:
@@ -18,14 +13,13 @@ def q_more_followers():
         if answer == 'a' or answer == 'b':
             return answer
         else:
-            continue
-
+            continue ##팔로워 예상 질문
 
 def compare(compare_a, compare_b):
     win_compare_a = True
     if int(compare_a['follower_count']) < int(compare_b['follower_count']):
         win_compare_a = False
-    return win_compare_a
+    return win_compare_a#내부적으로 누가 더 팔로워 높은지 계산
 
 def win_lose(more_follwers_answer, a_win):
     in_line = True
@@ -40,7 +34,7 @@ def win_lose(more_follwers_answer, a_win):
     else:
         print('\nWrong!')
         in_line = False
-        return in_line
+        return in_line#승패#승패
 
 def q_try_again():
     while True:
@@ -48,60 +42,72 @@ def q_try_again():
         if answer == 'y' or answer == 'n':
             return answer
         else:
-            continue
+            continue#다시하기 질문
 
-
-print(data.logo)
-print("\nWelcome to Higher Lower!\n\nGuess who's follower is higher or lower!")
+clear()
+print("\nWelcome to Higher Lower!\n\nGuess who's follower is higher or lower!") #greetings
 end = False
 while True:
     round = 1
     point = 0
-    compare_a = {}
-    data_2 = data.data
+    compare_a = {} #setting defualt
+    data_2 = data.data[:] #assigning different id to data2 so that can set defualt
     while end is False:
-        if round == 1:
+        if len(data_2) == 1: #end-of-list 1
+            pass
+        elif len(data_2) ==0: #end-of-list 2
+            print("\nEnd of list I have!")
+            break
+        elif round == 1: #only random gen a on first start
             print(data.logo)
             compare_a = gen_random_person(data_2, end)
             print(
-                f"\n{compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}"
+                f"\ncompare A : {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}"
             )
-        elif len(data_2) == 1:
-            pass
-        else:
-            print(data.logo)
+        else: #only print information if not first start
             print(
-                f"\n{compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}"
+                f"\ncompare A : {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}"
             )
+            
         print(data.vs)
 
-        compare_b = gen_random_person(data.data, end)
+        compare_b = gen_random_person(data_2, end)
         if compare_b == {}:
-            end = True
+            end = True #end if end-of-list
         else:
             print(
-                f"\n{compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}"
-            )
-        try:
-            a = int(compare_b['follower_count'])+1
-        except KeyError:
-            break
-
+                f"\nagainst B : {compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}"
+            ) # gen b
+ 
+        
         more_follwers_answer = q_more_followers()
+        print(data.logo)
         a_win = compare(compare_a, compare_b)
         in_line = win_lose(more_follwers_answer, a_win)
-
+        clear()
         if in_line:
             round+=1
-            compare_a = compare_b
+            point+=1
+            print(data.logo)
+            print(f'\nYour score is ... {point}')
+            compare_a = compare_b #b => a
             continue
-
-    print(f'\nYour score is: {point}!')
-    answer_try_again = q_try_again()
+        else:
+            round+=1
+            print('You lose!')
+            end = True
+    
+    print(f'\nYour final score is...{point}!')
+    
+    answer_try_again = q_try_again() #재시작
     if answer_try_again == 'y':
-        end = False
-        continue
+        end = False #end = False로 만들어서 루프가능
+        clear()
+        continue    
     else:
         break
+        
 
-print('Thank you for playing Higher and Lower!')
+clear()
+print(data.logo)
+print('\nThank you for playing Higher and Lower!')
