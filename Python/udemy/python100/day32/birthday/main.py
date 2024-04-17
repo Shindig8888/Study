36bc403a42238f0b257b521e3efdb3ed2c1ef10b
Shindig <hindig8888@proton.me>
2024-04-17 18:37:03 +0900

    change

--- Python/udemy/python100/day32/birthday/main.py
+++ Python/udemy/python100/day32/birthday/main.py
@@ -13,10 +13,9 @@ def open_and_write(name_value: str) -> list:
     #open random templates and return with list of letters which [name] is replaced with name of target
     random_letter_number = random.randint(1,3)
     with open(f'letter_templates/letter_{random_letter_number}.txt', 'r') as letter:
-        random_letter = letter.readlines()
-        random_letter[0] = random_letter[0].replace("[NAME]", name_value)
-    result_letter = ''.join(random_letter)
-    return result_letter
+        random_letter = letter.read()
+        random_letter = random_letter.replace("[NAME]", name_value)
+    return random_letter
 
 def send_email(email_value: str, letter: str):
     #send email
@@ -37,6 +36,7 @@ name_list = dataframe[dataframe['month']==month][dataframe['day']==day]['name'].
 email_list = dataframe[dataframe['month']==month][dataframe['day']==day]['email'].to_list()
 
 letters = [open_and_write(name) for name in name_list]
+print(letters)
 
 for index in range(0,len(name_list)):
     email = email_list[index]
