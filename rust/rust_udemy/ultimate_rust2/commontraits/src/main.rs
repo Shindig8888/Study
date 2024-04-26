01e6b9f73602975a725f683ad27a5d460fbed4a8
Shindig <hindig8888@proton.me>
2024-04-26 11:34:16 +0900

    common traits

---
+++ rust/rust_udemy/ultimate_rust2/commontraits/src/main.rs
@@ -0,0 +1,15 @@
+//derivable
+//Debug
+//Clone(int, float)
+//Copy(instead of move)
+
+//Defualt: __init__
+//..Default::default()
+
+//PartialEq / Eq
+
+//From / Into
+
+fn main() {
+    println!("Hello, world!");
+}
